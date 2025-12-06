56a5c7ee654e6ccd082ab04ac520e8ef98058834
Pedro <peboudoux@outlook.com>
2025-12-05 23:33:46 -0500

    added nvf

---
+++ home-manager/modules/nvf/default.nix
@@ -0,0 +1,15 @@
+{
+	programs.nvf = {
+		enable = true;
+
+		#settings go into settings (duh)
+		settings = {
+			vim.viAlias = false;
+			vim.vimAlias = true;
+			
+			vim.lsp = {
+				enable = true;
+			};
+		};
+	};
+}
