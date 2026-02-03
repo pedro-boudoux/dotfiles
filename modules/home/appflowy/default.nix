6efa86fb83a581b6d3aa1e0c7bc2eff4b2d96604
Pedro <peboudoux@outlook.com>
2026-02-03 14:51:00 -0500

    Use xdg.dataFile for AppFlowy themes to avoid symlinks

--- modules/home/appflowy/default.nix
+++ modules/home/appflowy/default.nix
@@ -1,16 +1,9 @@
 { config, pkgs, ... }: {
   # AppFlowy theme files
   # Themes are stored in ~/.local/share/appflowy/flowy_themes/
-  # and can be imported through the AppFlowy settings UI
-  home.file = {
-    ".local/share/appflowy/flowy_themes/Gruvbox-Dark/Gruvbox-Dark.dark.json" = {
-      text = builtins.readFile ./gruvbox-dark.json;
-      force = true;
-    };
-    
-    ".local/share/appflowy/flowy_themes/Gruvbox-Dark/Gruvbox-Dark.light.json" = {
-      text = builtins.readFile ./gruvbox-light.json;
-      force = true;
-    };
+  # Using xdg.dataFile which creates actual files in ~/.local/share
+  xdg.dataFile = {
+    "appflowy/flowy_themes/Gruvbox-Dark/Gruvbox-Dark.dark.json".text = builtins.readFile ./gruvbox-dark.json;
+    "appflowy/flowy_themes/Gruvbox-Dark/Gruvbox-Dark.light.json".text = builtins.readFile ./gruvbox-light.json;
   };
 }
